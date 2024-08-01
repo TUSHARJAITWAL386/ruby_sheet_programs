20ac6e8de6fd7d1ec3d41c158ed1c2dd7ed4271f
TUSHARJAITWAL386 <tushar.j@webkorps.com>
2024-08-02 01:22:38 +0530

    Ruby Programs

--- ruby1_check_number_positive_negative_zero.rb
+++ ruby1_check_number_positive_negative_zero.rb
@@ -1,16 +1,19 @@
-def check_number(n)
-    if n == 0
-        puts "Number is Zero"
-    elsif n < 0
-        puts "Number is Negative"
-    elsif n > 0
-        puts "Number is Positive"
-    else
-        puts "Invalid Number"
-    end
-end
-  
-print "Enter the number :-- "
-n = gets.chomp.to_i
 
-check_number(n)
+puts "Enter The Number :- "
+input = gets.chomp()
+
+if input.match?(/[a-zA-Z]/)
+  puts "Enter only numbers"
+else
+  n = input.to_i
+
+  if n > 0
+    puts "Number is positive"
+  elsif n < 0
+    puts "Number is negative"
+  elsif n == 0
+      puts "Number is zero"
+  else
+    puts "Invalid Number"
+  end
+end
